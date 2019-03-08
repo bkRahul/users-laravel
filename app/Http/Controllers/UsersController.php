a069f3912721c59edca2cc3384530a32eae68012
bkRahul <rahulsatya1068@gmail.com>
2019-03-08 23:43:49 +0530

    added store method to store user details and save it to database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,8 +15,18 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::all();
-        return $users;
+ 
+        return view('users.index', compact('users'));
+    }
+    public function store()
+    {
+        $user = new User;
+        
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = bcrypt( request('password') );
+        $user->save();
 
-        return view('users.index');
+        return back();
     }
 }
